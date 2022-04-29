24b85e0322ff31e930b2a79a0dba32503a6b25d9
Ramil Gabitov <ramil.g@jafton.com>
2022-04-29 17:22:27 +1000

    database environment variables

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -4,8 +4,8 @@ return [
         'db' => [
             'class' => 'yii\db\Connection',
             'dsn' => sprintf(
-                '%s:host=database;port=%d;dbname=%s;user=%s;password=%s',
-                getenv('DBMS'),
+                'pgsql:host=%s;port=%s;dbname=%s;user=%s;password=%s',
+                getenv('DB_HOST'),
                 getenv('DB_PORT'),
                 getenv('DB_NAME'),
                 getenv('DB_USER'),
